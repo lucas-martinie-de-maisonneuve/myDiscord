e00ac669084d74ea536bf46a9a500e96218894cf
Lucas Martinie de Maisonneuve <lucas.martinie-de-maisonneuve@laplateforme.io>
2024-02-06 12:21:07 +0100

    Création des fichiers server.py et user.py

---
+++ source/server.py
@@ -0,0 +1,5 @@
+from source.user import User
+
+class Server(User):
+    def __init__(self):
+        User().__init__()
---
+++ source/user.py
@@ -0,0 +1,9 @@
+class User:
+    def __init__(self):
+        pass
+
+    # Infos user 
+    # Authentification
+    # Création de compte
+    # Déconnexion
+    # envoi vers le serveur
