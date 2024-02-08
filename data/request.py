3ab53d6ac3a3c34abdcdff66c1292b321f061657
Lucas Martinie de Maisonneuve <lucas.martinie-de-maisonneuve@laplateforme.io>
2024-02-08 11:23:12 +0100

    Mise à jour des requetes

--- data/request.py
+++ data/request.py
@@ -1,90 +1,80 @@
-from database import Database
+from data.database import Database
+from datetime import datetime
+
 class Discord_Manager(Database):
     def __init__(self):
-        Database.__init__(self, 'localhost', 'root', '$~Bc4gB9', 'discord')
-        # Database.__init__(self, 'localhost', 'root', '$~Bc4gB9', 'discord')
-        # Database.__init__(self, 'localhost', 'root', '$~Bc4gB9', 'discord')
+        super().__init__('localhost', 'root', '$~Bc4gB9', 'discord')
         self.connect()
-        self.cursor = self.connection.cursor()
-        
-    # Ajout Utilisateur
-    def add_user(self, surname, name, pseudo, email, password,photo,id_role):
-        sql = "INSERT INTO product (surname, name, pseudo, email, password,photo,id_role) VALUES (%s, %s, %s, %s, %s)"
-        values = (surname, name, pseudo, email, password,photo,id_role)
-        self.cursor.execute(sql, values)
-        self.connection.commit()
 
-    # Nom Utilisateur
+    def add_user(self, surname, name, pseudo, email, password, photo, id_role):
+        sql = "INSERT INTO product (surname, name, pseudo, email, password, photo, id_role) VALUES (%s, %s, %s, %s, %s, %s, %s)"
+        values = (surname, name, pseudo, email, password, photo, id_role)
+        self.executeQuery(sql, values)
+
     def surname_user(self):
         sql = "SELECT surname FROM user"
-        self.cursor.execute(sql)
-        self.names = self.cursor.fetchall()
-        return self.names
-    
-    # Tout Utilisateurs
+        return self.fetch(sql)
+
     def display_user(self):
         sql = "SELECT * FROM user"
-        self.cursor.execute(sql)
-        self.users = self.cursor.fetchall()
-        return self.users
-    
-    # Nom Role Utilisateur 
+        return self.fetch(sql)
+
     def display_role_name(self):
         sql = "SELECT role.name FROM role LEFT JOIN user ON user.id_role = role.id"
-        self.cursor.execute(sql)
-        self.name_role = self.cursor.fetchall()
-        return self.name_role
-    
-    # Ajout Categorie
-    def add_category(self,name,intro):
+        return self.fetch(sql)
+
+    def add_category(self, name, intro):
         sql = "INSERT INTO category (name, intro) VALUES (%s, %s)"
         values = (name, intro)
-        self.cursor.execute(sql, values)
-        self.connection.commit()
-    
-    # Toutes Categories
+        self.executeQuery(sql, values)
+
     def display_category(self):
         sql = "SELECT * FROM category"
-        self.cursor.execute(sql)
-        self.categorys = self.cursor.fetchall()
-        print(self.categorys)
-        return self.categorys
-    
-    # Ajout Channel
-    def add_channel(self,name,status,communication,id_category):
-        sql = "INSERT INTO channel (name,status,communication,id_category) VALUES (%s, %s,%s, %s)"
-        values = (name,status,communication,id_category)
-        self.cursor.execute(sql, values)
-        self.connection.commit()
+        return self.fetch(sql)
 
-    # Toutes Channels
+    def add_channel(self, name, status, communication, id_category):
+        sql = "INSERT INTO channel (name, status, communication, id_category) VALUES (%s, %s, %s, %s)"
+        values = (name, status, communication, id_category)
+        self.executeQuery(sql, values)
+
     def display_channel(self):
         sql = "SELECT * FROM channel"
-        self.cursor.execute(sql)
-        self.channels = self.cursor.fetchall()
-        return self.channels
-    
-    # Supprimer User
+        return self.fetch(sql)
+
     def delete_user(self, id):
         sql = "DELETE FROM user WHERE id = %s"
         values = (id,)
-        self.cursor.execute(sql, values)
-        self.connection.commit()
-        
-    # Supprimer Category
+        self.executeQuery(sql, values)
+
     def delete_category(self, id):
         sql = "DELETE FROM category WHERE id = %s"
         values = (id,)
-        self.cursor.execute(sql, values)
-        self.connection.commit()
-        
-    # Supprimer Channel
+        self.executeQuery(sql, values)
+
     def delete_channel(self, id):
         sql = "DELETE FROM channel WHERE id = %s"
         values = (id,)
-        self.cursor.execute(sql, values)
-        self.connection.commit()
-    
+        self.executeQuery(sql, values)
+
+    def check_credentials(self, nickname, password):
+        sql = "SELECT * FROM user WHERE pseudo = %s AND password = %s"
+        values = (nickname, password)
+        user = self.fetch(sql, values)
+        return user is not None  
+
+    def save_message(self, name, message, id_channel):
+        time = datetime.now()
+        sql = "INSERT INTO message (name, time, message, id_channel) VALUES (%s, %s, %s, %s)"
+        values = (name, time, message, id_channel)
+        self.executeQuery(sql, values)
+
+    def get_message(self):
+        sql = "SELECT * FROM message"
+        return self.fetch(sql)
+
+    def close_connection(self):
+        self.disconnect()
+
     # Modifier Role
     # def modify_role(self, user_id, id_role):
     #     set_clause = ", ".join([f"{key} = '{value}'" for key, value in new_product.items()])
@@ -92,10 +82,6 @@ class Discord_Manager(Database):
     #     self.cursor.execute(sql, (product_id,))
     #     self.connection.commit()
     
-    def close_connection(self):
-        self.cursor.close()
-        self.connection.close()
-
 manager = Discord_Manager()
 manager.display_category()
 # manager.add_user("surname", "name", "pseudo", "email", "password","photo","id_role")
