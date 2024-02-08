f590a9cba3f872b954527a5ffb9aabd1a84af811
ines-lorquet <ines.lorquet@laplateforme.io>
2024-02-08 16:56:34 +0100

    Ajout du hoover sur les cercles des avatars des profils

--- inscription.py
+++ inscription.py
@@ -18,6 +18,37 @@ class Inscription(Element,Screen):
         self.profil2_cercle = pygame.draw.circle(self.Window, self.black, (530, 150), 50)
         self.profil3_cercle = pygame.draw.circle(self.Window, self.black, (680, 150), 50)
         self.profil4_cercle = pygame.draw.circle(self.Window, self.black, (830, 150), 50)
+        self.p_profil2 = self.profil2_cercle
+        if self.is_mouse_over_button(self.p_profil2):
+            self.profil2_cercle = pygame.draw.circle(self.Window, self.black, (530, 150), 50)
+            self.hoover_profil2_cercle = pygame.draw.circle(self.Window, self.grey1, (530, 150), 50, width=2) 
+        else:
+            self.profil2_cercle = pygame.draw.circle(self.Window, self.black, (530, 150), 50)
+            self.hoover_profil2_cercle = pygame.draw.circle(self.Window, self.grey2, (530, 150), 50, width=2) 
+        
+        self.p_profil3 = self.profil3_cercle
+        if self.is_mouse_over_button(self.p_profil3):
+            self.profil3_cercle = pygame.draw.circle(self.Window, self.black, (680, 150), 50)
+            self.hoover_profil3_cercle = pygame.draw.circle(self.Window, self.grey1, (680, 150), 50, width=2)
+        else:
+            self.profil3_cercle = pygame.draw.circle(self.Window, self.black, (680, 150), 50)
+            self.hoover_profil3_cercle = pygame.draw.circle(self.Window, self.grey2, (680, 150), 50, width=2)
+
+        self.p_profil4 = self.profil4_cercle
+        if self.is_mouse_over_button(self.p_profil4):
+            self.profil4_cercle = pygame.draw.circle(self.Window, self.black, (830, 150), 50)
+            self.hoover_profil4_cercle= pygame.draw.circle(self.Window, self.grey1, (830, 150), 50, width=2) 
+        else:
+            self.profil4_cercle = pygame.draw.circle(self.Window, self.black, (830, 150), 50)
+            self.hoover_profil4_cercle= pygame.draw.circle(self.Window, self.grey2, (830, 150), 50, width=2) 
+
+        self.p_profil1 = self.profil1_cercle
+        if self.is_mouse_over_button(self.p_profil1):
+            self.profil1_cercle = pygame.draw.circle(self.Window, self.black, (380, 150), 50)
+            self.hoover_profil1_cercle = pygame.draw.circle(self.Window, self.grey1, (380, 150), 50, width=2) 
+        else:
+            self.profil1_cercle = pygame.draw.circle(self.Window, self.black, (380, 150), 50)
+            self.hoover_profil1_cercle = pygame.draw.circle(self.Window, self.grey2, (380, 150), 50, width=2) 
     def inscription_run(self):
         running = True
         while running:
