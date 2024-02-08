8eb5b745452c2e9bfc7dbb192d355ee087764c06
ines-lorquet <ines.lorquet@laplateforme.io>
2024-02-08 16:55:56 +0100

    Création des cercles pour les avatars profils

--- inscription.py
+++ inscription.py
@@ -13,12 +13,16 @@ class Inscription(Element,Screen):
         self.Window.fill(self.white)
         self.rect_full(self.grey3, 600, 355, 600, 580, 5)
         self.rect_border(self.grey2, 600, 355, 600, 580, 2, 5)
-    
+    def profil_hoover(self):
+        self.profil1_cercle = pygame.draw.circle(self.Window, self.black, (380, 150), 50)
+        self.profil2_cercle = pygame.draw.circle(self.Window, self.black, (530, 150), 50)
+        self.profil3_cercle = pygame.draw.circle(self.Window, self.black, (680, 150), 50)
+        self.profil4_cercle = pygame.draw.circle(self.Window, self.black, (830, 150), 50)
     def inscription_run(self):
         running = True
         while running:
             self.form()
-            
+            self.profil_hoover()
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     running = False
