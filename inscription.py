45188823789b806436f7f89ceb0f98a528464841
ines-lorquet <ines.lorquet@laplateforme.io>
2024-02-09 10:46:45 +0100

    Placement des traits blanc pour option log in

--- inscription.py
+++ inscription.py
@@ -24,72 +24,75 @@ class Inscription(Element,Screen):
         self.rect_border(self.grey2, 600, 355, 600, 580, 2, 5)
         
         # Pseudo
-        self.pseudo_rect = self.rect_full(self.grey2, 600, 260, 400, 40, 5) 
-        self.button("pseudo", 600, 260, 400, 40, self.grey2, self.grey2, self.grey2, self.grey2,self.pseudo, self.font2, self.white, 15, 4, 5)
+        self.pseudo_rect = self.rect_full(self.grey2, 600, 240, 400, 40, 5) 
+        self.button("pseudo", 600, 240, 400, 40, self.grey2, self.grey2, self.grey2, self.grey2,self.pseudo, self.font2, self.white, 15, 4, 5)
         
         # Email
-        self.email_rect = self.rect_full(self.grey2, 600, 320, 400, 40, 5)
-        self.button("email", 600, 320, 400, 40, self.grey2, self.grey2, self.grey2, self.grey2,self.email, self.font2, self.white, 15, 4, 5)
+        self.email_rect = self.rect_full(self.grey2, 600, 300, 400, 40, 5)
+        self.button("email", 600, 300, 400, 40, self.grey2, self.grey2, self.grey2, self.grey2,self.email, self.font2, self.white, 15, 4, 5)
         
          # Prenom
-        self.surname_rect = self.rect_full(self.grey2, 600, 380, 400, 40, 5)
-        self.button("name", 600, 380, 400, 40, self.grey2, self.grey2, self.grey2, self.grey2,self.surname, self.font2, self.white, 15, 4, 5)
+        self.surname_rect = self.rect_full(self.grey2, 600, 360, 400, 40, 5)
+        self.button("name", 600, 360, 400, 40, self.grey2, self.grey2, self.grey2, self.grey2,self.surname, self.font2, self.white, 15, 4, 5)
         
         # Nom
-        self.name_rect = self.rect_full(self.grey2, 600, 440, 400, 40, 5)
-        self.button("name", 600, 440, 400, 40, self.grey2, self.grey2, self.grey2, self.grey2,self.name, self.font2, self.white, 15, 4, 5)
+        self.name_rect = self.rect_full(self.grey2, 600, 420, 400, 40, 5)
+        self.button("name", 600, 420, 400, 40, self.grey2, self.grey2, self.grey2, self.grey2,self.name, self.font2, self.white, 15, 4, 5)
         
         # Mot De Passe
-        self.password_rect = self.rect_full(self.grey2, 600, 500, 400, 40, 5)
-        self.button("password", 600, 500, 400, 40, self.grey2, self.grey2, self.grey2, self.grey2,self.password, self.font2, self.white, 15, 4, 5)
+        self.password_rect = self.rect_full(self.grey2, 600, 480, 400, 40, 5)
+        self.button("password", 600, 480, 400, 40, self.grey2, self.grey2, self.grey2, self.grey2,self.password, self.font2, self.white, 15, 4, 5)
         
-        self.sign_in = self.button("connexion", 600, 580, 350, 50, self.blue, self.blue, self.blue1, self.blue1,"Sign In", self.font1, self.white, 15, 4, 5)
+        self.sign_in = self.button("connexion", 600, 560, 350, 50, self.blue, self.blue, self.blue1, self.blue1,"Sign In", self.font1, self.white, 15, 4, 5)
+        
+        pygame.draw.line(self.Window, self.grey1, (630, 600), (770, 600), 1)
+        pygame.draw.line(self.Window, self.grey1, (430, 600), (564, 600), 1)
+        
+
         
     def profil_screen(self):
-        self.img_center("profil1",380,150,90,65,"new_profil/profil1")
-        self.img_center("profil2",530,150,100,80,"new_profil/profil2")
-        self.img_center("profil3",680,150,80,60,"new_profil/profil3")
-        self.img_center("profil4",830,150,100,90,"new_profil/profil4")
+        self.img_center("profil1",380,140,90,65,"new_profil/profil1")
+        self.img_center("profil2",530,140,100,80,"new_profil/profil2")
+        self.img_center("profil3",680,140,80,60,"new_profil/profil3")
+        self.img_center("profil4",830,140,100,90,"new_profil/profil4")
 
     def profil_hoover(self):
-        self.profil1_cercle = pygame.draw.circle(self.Window, self.black, (380, 150), 50)
-        self.profil2_cercle = pygame.draw.circle(self.Window, self.black, (530, 150), 50)
-        self.profil3_cercle = pygame.draw.circle(self.Window, self.black, (680, 150), 50)
-        self.profil4_cercle = pygame.draw.circle(self.Window, self.black, (830, 150), 50)
-        self.p_profil2 = self.profil2_cercle
+        self.profil1_cercle = pygame.draw.circle(self.Window, self.black, (380, 140), 50)
+        self.profil2_cercle = pygame.draw.circle(self.Window, self.black, (530, 140), 50)
+        self.profil3_cercle = pygame.draw.circle(self.Window, self.black, (680, 140), 50)
+        self.profil4_cercle = pygame.draw.circle(self.Window, self.black, (830, 140), 50)
         
+        self.p_profil2 = self.profil2_cercle
         if self.is_mouse_over_button(self.p_profil2):
-            self.profil2_cercle = pygame.draw.circle(self.Window, self.black, (530, 150), 50)
-            self.hoover_profil2_cercle = pygame.draw.circle(self.Window, self.grey1, (530, 150), 50, width=2)
-        # elif self.photo == 2:
-   
+            self.profil2_cercle = pygame.draw.circle(self.Window, self.black, (530, 140), 50)
+            self.hoover_profil2_cercle = pygame.draw.circle(self.Window, self.grey1, (530, 140), 50, width=2)   
         else:
-            self.profil2_cercle = pygame.draw.circle(self.Window, self.black, (530, 150), 50)
-            self.hoover_profil2_cercle = pygame.draw.circle(self.Window, self.grey2, (530, 150), 50, width=2) 
+            self.profil2_cercle = pygame.draw.circle(self.Window, self.black, (530, 140), 50)
+            self.hoover_profil2_cercle = pygame.draw.circle(self.Window, self.grey2, (530, 140), 50, width=2) 
 
         self.p_profil3 = self.profil3_cercle
         if self.is_mouse_over_button(self.p_profil3):
-            self.profil3_cercle = pygame.draw.circle(self.Window, self.black, (680, 150), 50)
-            self.hoover_profil3_cercle = pygame.draw.circle(self.Window, self.grey1, (680, 150), 50, width=2)
+            self.profil3_cercle = pygame.draw.circle(self.Window, self.black, (680, 140), 50)
+            self.hoover_profil3_cercle = pygame.draw.circle(self.Window, self.grey1, (680, 140), 50, width=2)
         else:
-            self.profil3_cercle = pygame.draw.circle(self.Window, self.black, (680, 150), 50)
-            self.hoover_profil3_cercle = pygame.draw.circle(self.Window, self.grey2, (680, 150), 50, width=2)
+            self.profil3_cercle = pygame.draw.circle(self.Window, self.black, (680, 140), 50)
+            self.hoover_profil3_cercle = pygame.draw.circle(self.Window, self.grey2, (680, 140), 50, width=2)
 
         self.p_profil4 = self.profil4_cercle
         if self.is_mouse_over_button(self.p_profil4):
-            self.profil4_cercle = pygame.draw.circle(self.Window, self.black, (830, 150), 50)
-            self.hoover_profil4_cercle= pygame.draw.circle(self.Window, self.grey1, (830, 150), 50, width=2) 
+            self.profil4_cercle = pygame.draw.circle(self.Window, self.black, (830, 140), 50)
+            self.hoover_profil4_cercle= pygame.draw.circle(self.Window, self.grey1, (830, 140), 50, width=2) 
         else:
-            self.profil4_cercle = pygame.draw.circle(self.Window, self.black, (830, 150), 50)
-            self.hoover_profil4_cercle= pygame.draw.circle(self.Window, self.grey2, (830, 150), 50, width=2) 
+            self.profil4_cercle = pygame.draw.circle(self.Window, self.black, (830, 140), 50)
+            self.hoover_profil4_cercle= pygame.draw.circle(self.Window, self.grey2, (830, 140), 50, width=2) 
 
         self.p_profil1 = self.profil1_cercle
         if self.is_mouse_over_button(self.p_profil1):
-            self.profil1_cercle = pygame.draw.circle(self.Window, self.black, (380, 150), 50)
-            self.hoover_profil1_cercle = pygame.draw.circle(self.Window, self.grey1, (380, 150), 50, width=2) 
+            self.profil1_cercle = pygame.draw.circle(self.Window, self.black, (380, 140), 50)
+            self.hoover_profil1_cercle = pygame.draw.circle(self.Window, self.grey1, (380, 140), 50, width=2) 
         else:
-            self.profil1_cercle = pygame.draw.circle(self.Window, self.black, (380, 150), 50)
-            self.hoover_profil1_cercle = pygame.draw.circle(self.Window, self.grey2, (380, 150), 50, width=2) 
+            self.profil1_cercle = pygame.draw.circle(self.Window, self.black, (380, 140), 50)
+            self.hoover_profil1_cercle = pygame.draw.circle(self.Window, self.grey2, (380, 140), 50, width=2) 
             
     def inscription_run(self):
         running = True
