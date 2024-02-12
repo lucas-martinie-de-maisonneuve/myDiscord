702824799c8861fb9550b3bbe3d41991f088ad20
ines-lorquet <ines.lorquet@laplateforme.io>
2024-02-12 15:01:47 +0100

    Création des méthode vide

--- principal_page.py
+++ principal_page.py
@@ -5,17 +5,25 @@ from source.pygame_manager.screen import Screen
 # from data.discord_manager import Discord_Manager
 
 class Home(Element, Screen):
-    
+
     def __init__(self):
         Screen.__init__(self)
         Element.__init__(self)
         pygame.init()
+        self.principal_page_run = True
 
-    
+    def serveur_page(self):
+        pass
+
+    def channel_page(self):
+        pass
+
+    def chat_page(self):
+        pass
+
     def principal_page_run(self):
-
-        principal_page_run = True
-        while principal_page_run :
+        self.principal_page_run = True
+        while self.principal_page_run :
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
-                        principal_page_run = False
+                        self.principal_page_run = False
