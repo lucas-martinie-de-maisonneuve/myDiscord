2d467f98255a2c2c56f75ed179fa80375ef3554d
ines-lorquet <ines.lorquet@laplateforme.io>
2024-02-13 14:17:41 +0100

    Mise en place de la premiere section

--- main_page.py
+++ main_page.py
@@ -14,7 +14,29 @@ class Main_page(Element, Screen,Discord_Manager):
         self.rect_full(self.grey4, 645, 35, 1110, 70, 0)
 
     def FirstSection(self):
-        self.rect_full(self.grey8, 45, 350, 90, 700, 0)
+        # First section background color
+        self.rect_full(self.grey7, 45, 350, 90, 700, 0)
+
+        # Main Logo    
+        self.image_not_center("Logo prinicpal", 2, 0, 90, 90,"main_page/main_page3")       
+
+        # Hover server
+        self.cercle1 = pygame.draw.circle(self.Window, self.grey7, (44, 120), 35)     
+        if self.is_mouse_over_button(self.cercle1):      
+            self.img_center("Logo prinicpal", 44, 120, 75, 75,"main_page/main_page2")
+            self.img_center("Logo prinicpal", 44, 120, 115, 115,"main_page/main_page4")
+        else:          
+            self.img_center("Logo prinicpal", 44, 120, 85, 85,"main_page/main_page2")
+        
+        # Hover settings
+        self.cercle2 = pygame.draw.circle(self.Window, self.grey7, (45, 655), 35)
+        if self.is_mouse_over_button(self.cercle2):           
+            self.img_center("Logo prinicpal", 44, 640, 115, 115,"main_page/main_page4")   
+            self.img_center("settings", 44, 635, 85, 85,"main_page/main_page5")
+            self.img_center("settings", 55, 665, 30, 30,"main_page/main_page5")
+        else:      
+            self.img_center("settings", 44, 635, 90, 90,"main_page/main_page5")
+            self.img_center("settings", 55, 665, 35, 35,"main_page/main_page5")
 
     def SecondSection(self):
         self.rect_full(self.grey7, 220, 385, 260, 630, 0)
