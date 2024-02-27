0005a99f68c2566aa65696a4fca92316d69e6a5e
vanny-laure-lamorte <vanny.lamorte@laplateforme.io>
2024-02-27 12:18:51 +0100

    Modifier la database

--- data/myDiscord.sql
+++ data/myDiscord.sql
@@ -23,9 +23,7 @@ INSERT INTO user(surname, name, pseudo, email, password, photo, id_role) VALUES
 ('Claire','Lamorte', 'Cla Loup', 'claire.lamorte@laplateforme.io', 'ClaireLamorte1234!', 3, 2),
 ('Elise','Martinie', 'Hey lee02', 'elise.martinie@laplateforme.io', 'EliseMartinie01234!', 2, 2),
 ('Julien','Beaurain', 'Ju Bond', 'julien.beaurain@laplateforme.io', 'JulienBeaurain01234!', 1, 2),
-('Alex','Philipot', 'Poupinou', 'alexandre.philipot@laplateforme.io', 'Alex.Philipot01234!', 4, 2)
-
-;
+('Alex','Philipot', 'Poupinou', 'alexandre.philipot@laplateforme.io', 'Alex.Philipot01234!', 4, 2);
 
 CREATE TABLE role (
     id INT PRIMARY KEY AUTO_INCREMENT,
@@ -124,13 +122,6 @@ CREATE TABLE user (
     photo INT, 
     id_role INT
 );
-
-INSERT INTO user(surname, name, pseudo, email, password, photo, id_role) VALUES
-('Ines','Lorquet', 'Inessa', 'ines.lorquet@laplateforme.io', '586447cae9b58ff7e2c0a2b3980caf7f2ac5984bfe92cbb99eb0a5f0a702914d', 1, 1),
-('Lucas','Martinie', 'Lucassa', 'lucas.martinie@laplateforme.io', 'fc96020f4fc3fbbe01b03f6c1ef101a57110b6844511567e2cca087e02c0d4bb', 2, 1),
-('Vanny','Lamorte', 'Vannyssa', 'vanny.lamorte@laplateforme.io', '5c5bdc4a2ad0deadbd40affb8fe0e359ff6fb3402a38b2a8addbdee2b802d1b5', 3, 2),
-('Jacques','Dubois', 'Jack', 'jacques.dubois@laplateforme.io', '2f5cede731389f72b1c679168da2258bb21420e12f547c89ab6cd769ba5e8087', 4, 2);
-
 
 
 CREATE TABLE password (
